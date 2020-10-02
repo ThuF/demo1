f0fbc16e5ef6d76ff8f24a62677d1010218daa94
Yordan Pavlov <ThuF@users.noreply.github.com>
2020-10-02 15:09:06 +0300

    Update demo.js

--- drago/demo.js
+++ drago/demo.js
@@ -1,5 +1,5 @@
 var response = require("http/v4/response");
 
-response.println("Hello World from Eclipse Dirigible!");
+response.println("Hello GitHub World from Eclipse Dirigible!");
 response.flush();
 response.close();
